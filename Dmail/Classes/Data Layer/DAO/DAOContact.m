7f6ccad51f547a302f834d9701405fddf115aead
Karen Petrosyan <kpetrosyan@iconapps.com>
2015-07-10 18:30:17 +0400

    working on contact auto-complete UI.

--- Dmail/Classes/Data Layer/DAO/DAOContact.m
+++ Dmail/Classes/Data Layer/DAO/DAOContact.m
@@ -54,7 +54,7 @@ const NSInteger contactsUpdateTime = 12;
     
     NSMutableArray *arrayContacts = [[NSMutableArray alloc] init];
     if(name.length > 0) {
-        NSPredicate *predicate =  [NSPredicate predicateWithFormat:@"firstName CONTAINS [c] %@ OR lastName CONTAINS [c] %@ OR email CONTAINS [c] %@", name, name, name];
+        NSPredicate *predicate =  [NSPredicate predicateWithFormat:@"firstName BEGINSWITH[c] %@ OR lastName BEGINSWITH[c] %@ OR email BEGINSWITH[c] %@", name, name, name];
         RLMResults *result = [RMModelContact objectsWithPredicate:predicate];
         for (RMModelContact *rmModel in result) {
             ContactModel *model = [[ContactModel alloc] initWithRMModel:rmModel];
@@ -109,7 +109,7 @@ const NSInteger contactsUpdateTime = 12;
             for (NSDictionary *emailDict in emailArray) {
                 if ([[emailDict allKeys] containsObject:@"address"]) {
                     email = emailDict[@"address"];
-                    break;
+                    NSLog(@"email ==== %@", email);
                 }
             }
         }
@@ -117,28 +117,34 @@ const NSInteger contactsUpdateTime = 12;
             NSDictionary *nameDict = dict[@"gd$name"];
             NSDictionary *fullNameDict = nameDict[@"gd$fullName"];
             fullName = fullNameDict[@"$t"];
+            NSLog(@"fullName ==== %@", fullName);
             
             NSDictionary *lastNameDict = nameDict[@"gd$familyName"];
             lastName = lastNameDict[@"$t"];
+            NSLog(@"lastName ==== %@", lastName);
             
             NSDictionary *firstNameDict = nameDict[@"gd$givenName"];
             firstName = firstNameDict[@"$t"];
+            NSLog(@"firstName ==== %@", firstName);
         }
         if ([[dict allKeys] containsObject:@"id"]) {
             NSDictionary *idDict = dict[@"id"];
             contactId = idDict[@"$t"];
             NSArray *array = [contactId componentsSeparatedByString:@"/"];
             contactId = [array lastObject];
+            NSLog(@"contactId ==== %@", contactId);
         }
         if ([[dict allKeys] containsObject:@"link"]) {
             NSArray *hrefArray = dict[@"link"];
             for (NSDictionary *hrefDict in hrefArray) {
                 if ([[hrefDict allKeys] containsObject:@"href"]) {
                     urlPhoto = hrefDict[@"href"];
+                    NSLog(@"urlPhoto ==== %@", urlPhoto);
                     break;
                 }
             }
         }
+        NSLog(@"===========================  ==========================");
         
         ContactModel *model = [[ContactModel alloc] initWithEmail:email fullName:fullName firstName:firstName lastName:lastName contactId:contactId urlPhoto:urlPhoto];
         if (model) {
--- Dmail/Classes/Presentation Layer/Views/Cells/Contacts/ContactCell.m
+++ Dmail/Classes/Presentation Layer/Views/Cells/Contacts/ContactCell.m
@@ -31,29 +31,45 @@
 
 - (void)configureCellWithContactModel:(ContactModel *)contactModel searchText:(NSString *)searchtext{
     
-    NSString * strEmail = contactModel.email;
-    strEmail = [strEmail lowercaseString];
-    if (strEmail.length > 1) {
-        NSRange range = [strEmail rangeOfString:searchtext];
-        if (range.location != NSNotFound) {
-            NSMutableAttributedString * attributedEmail = [[NSMutableAttributedString alloc] initWithString:contactModel.email];
-            [attributedEmail addAttribute: NSFontAttributeName value:[UIFont fontWithName:@"ProximaNova-Semibold" size:16] range:range];
-            [attributedEmail addAttribute: NSForegroundColorAttributeName value:[UIColor redColor] range:range]; // if needed
-            [self.labelEmail setAttributedText: attributedEmail];
-        }
-    }
+    self.labelEmail.text = contactModel.email;
+    self.labelFullName.text = [NSString stringWithFormat:@"%@ %@", contactModel.firstName, contactModel.lastName];
     
-    NSString * strFullName = contactModel.fullName;
-    strFullName = [strFullName lowercaseString];
-    if (strFullName.length > 1) {
-        NSRange range = [strFullName rangeOfString:searchtext];
-        if (range.location != NSNotFound) {
-            NSMutableAttributedString * attributedEmail = [[NSMutableAttributedString alloc] initWithString:contactModel.fullName];
-            [attributedEmail addAttribute: NSFontAttributeName value:[UIFont fontWithName:@"ProximaNova-Semibold" size:16] range:range];
-            [attributedEmail addAttribute: NSForegroundColorAttributeName value:[UIColor redColor] range:range]; // if needed
-            [self.labelFullName setAttributedText: attributedEmail];
-        }
-    }
+//    [self.labelEmail setAttributedText: attributedEmail];
+//    NSString * strEmail = contactModel.email;
+//    strEmail = [strEmail lowercaseString];
+//    if (strEmail.length > 1) {
+//        NSRange range = [strEmail rangeOfString:searchtext];
+//        if (range.location != NSNotFound) {
+//            NSMutableAttributedString * attributedEmail = [[NSMutableAttributedString alloc] initWithString:contactModel.email];
+//            [attributedEmail addAttribute: NSFontAttributeName value:[UIFont fontWithName:@"ProximaNova-Semibold" size:16] range:range];
+//            [attributedEmail addAttribute: NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+//            [self.labelEmail setAttributedText: attributedEmail];
+//        }
+//    }
+//    
+//    NSString * strFirstName = contactModel.firstName;
+//    strFirstName = [strFirstName lowercaseString];
+//    if (strFirstName.length > 1) {
+//        NSRange range = [strFirstName rangeOfString:searchtext];
+//        if (range.location != NSNotFound) {
+//            NSMutableAttributedString * attributedEmail = [[NSMutableAttributedString alloc] initWithString:contactModel.fullName];
+//            [attributedEmail addAttribute: NSFontAttributeName value:[UIFont fontWithName:@"ProximaNova-Semibold" size:16] range:range];
+//            [attributedEmail addAttribute: NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+//            [self.labelFullName setAttributedText: attributedEmail];
+//        }
+//    }
+//    
+//    NSString * strlastName = contactModel.lastName;
+//    strlastName = [strlastName lowercaseString];
+//    if (strlastName.length > 1) {
+//        NSRange range = [strlastName rangeOfString:searchtext];
+//        if (range.location != NSNotFound) {
+//            NSMutableAttributedString * attributedEmail = [[NSMutableAttributedString alloc] initWithString:contactModel.fullName];
+//            [attributedEmail addAttribute: NSFontAttributeName value:[UIFont fontWithName:@"ProximaNova-Semibold" size:16] range:range];
+//            [attributedEmail addAttribute: NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+//            [self.labelFullName setAttributedText: attributedEmail];
+//        }
+//    }
 }
 
 @end
