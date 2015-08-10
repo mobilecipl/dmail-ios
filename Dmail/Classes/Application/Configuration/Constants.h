fb60b62fdeb2763741f3c35ae2c131f030aeceef
Karen Petrosyan <kpetrosyan@iconapps.com>
2015-08-10 10:51:03 +0400

    Removed unused constants.

--- Dmail/Classes/Application/Configuration/Constants.h
+++ Dmail/Classes/Application/Configuration/Constants.h
@@ -11,13 +11,6 @@
 
 typedef void(^mainCompletionBlock)(NSDictionary *requestData, NSInteger statusCode);
 
-#pragma mark - Inbox Constants
-static NSString * SenderName = @"senderName";
-static NSString * MessageSubject = @"messageSubject";
-static NSString * MessageDate = @"messageDate";
-static NSString * MessageGmailUniqueId = @"messageGmailUniqueId";
-
-
 #pragma mark - Messages
 static NSString * kGlobalAlertMessageBasicFailure = @"Something went wrong\nPlease try again.";
 static NSString * kGlobalAlertMessageRequestTimeOutReplaceler = @"Looks like there's a bad connection. Please try again in a minute or so!";
@@ -27,25 +20,16 @@ static NSString * const kErrorMessageNoServer = @"Please try later\nWe are unabl
 
 
 #pragma mark - Notifications
-static NSString * const NotificationMenuButton = @"NotificationMenuButton";
-static NSString * const NotificationInbox = @"NotificationInbox";
-static NSString * const NotificationSent = @"NotificationSent";
-static NSString * const NotificationMessageInbox = @"NotificationMessageInbox";
-static NSString * const NotificationMessageSent = @"NotificationMessageSent";
 static NSString * const NotificationNewMessageFetched = @"NotificationNewMessageFetched";
 static NSString * const NotificationNewMessageSent = @"NotificationNewMessageSent";
 static NSString * const NotificationNewMessageSentError = @"NotificationNewMessageSentError";
-static NSString * const NotificationUpdateContacts = @"NotificationUpdateContacts";
 static NSString * const NotificationGMailUniqueFetched = @"NotificationGMailUniqueFetched";
 static NSString * const NotificationGMailMessageFetched = @"NotificationGMailMessageFetched";
-
 static NSString * const NotificationDestroySuccess = @"NotificationDestroySuccess";
 static NSString * const NotificationDestroyFailed = @"NotificationDestroyFailed";
 static NSString * const NotificationRevokeSuccess = @"NotificationRevokeSuccess";
 static NSString * const NotificationRevokeFailed = @"NotificationRevokeFailed";
-
 static NSString * const NotificationGetDecryptedMessage = @"NotificationGetDecryptedMessage";
-
 static NSString * const NotificationMenuOpened = @"menuOpened";
 static NSString * const NotificationMenuClosed = @"menuClosed";
 
@@ -75,44 +59,5 @@ typedef NS_ENUM(NSInteger, MessageStatus) {
 static NSInteger kMessageUpdateTime = 10;
 static NSInteger kMessageGetCount = 30;
 static NSInteger kProfileImageSize = 50;
-
-
-#pragma mark - Request Keys
-#pragma mark - Get Message List
-static NSString * const Recipients = @"recipients";
-
-static NSString * const Count = @"count";
-static NSString * const RecipientType = @"recipient_type";
-static NSString * const RecipientEmail = @"recipient_email";
-static NSString * const EncryptedMessage = @"encrypted_message";
-static NSString * const SenderEmail = @"sender_email";
-static NSString * const Bottom = @"bottom";
-static NSString * const Subject = @"Subject";
-static NSString * const Body = @"body";
-static NSString * const InternalDate = @"internalDate";
-static NSString * const Payload = @"payload";
-static NSString * const Headers = @"headers";
-static NSString * const Name = @"name";
-static NSString * const Value = @"value";
-static NSString * const From = @"From";
-static NSString * const To = @"To";
-static NSString * const Cc = @"Cc";
-static NSString * const Bcc = @"Bcc";
-static NSString * const Email = @"email";
-static NSString * const Message_Id = @"Message-Id";
-static NSString * const Status = @"status";
-static NSString * const PublicKey = @"PublicKey";
-static NSString * const Snippet = @"snippet";
-static NSString * const DmailId = @"DmailId";
-
-//Dmail Keys
-static NSString * const Access = @"access";
-static NSString * const MessageId = @"messageId";
-static NSString * const MessageIdentifier = @"messageIdentifier";
-static NSString * const Position = @"position";
-static NSString * const Type = @"type";
-
-static NSString * const AccessTypeGaranted = @"GRANTED";
-static NSString * const AccessTypeRevoked = @"REVOKED";
 
 #endif
