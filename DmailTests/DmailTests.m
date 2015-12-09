c5f619db8242fa649e0867e65b32a32b76c379c0
Ankita Kalangutkar <ankita.kalangutkar@science-inc.com>
2015-12-10 01:42:03 +0530

    installed pod

--- DmailTests/DmailTests.m
+++ DmailTests/DmailTests.m
@@ -2,17 +2,15 @@
 //  DmailTests.m
 //  DmailTests
 //
-//  Created by Karen Petrosyan on 6/12/15.
+//  Created by Ankita Kalangutkar on 12/10/15.
 //  Copyright (c) 2015 Karen Petrosyan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import "NetworkMessage.h"
-
 @interface DmailTests : XCTestCase
-@property (nonatomic, strong) NetworkMessage *networkMessage;
+
 @end
 
 @implementation DmailTests
@@ -20,7 +18,6 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    _networkMessage = [[NetworkMessage alloc] init];
 }
 
 - (void)tearDown {
@@ -28,30 +25,9 @@
     [super tearDown];
 }
 
-- (void)testSendEncryptedMessage {
+- (void)testExample {
     // This is an example of a functional test case.
-    
-    NSURL *URL = [NSURL URLWithString:@"http://nshipster.com/"];
-    NSString *description = [NSString stringWithFormat:@"GET %@", URL];
-    XCTestExpectation *expectation = [self expectationWithDescription:description];
-
-    
-    NSString *message = @"encrypted message";
-    NSString *senderEmail = @"senderEmail";
-    
-    [self.networkMessage sendEncryptedMessage:message senderEmail:senderEmail completionBlock:^(id data, ErrorDataModel *error) {
-        
-        NSLog(@"sendEncryptedMessage data : %@, error : %@", data, error);
-        XCTAssertNotNil(data, "data should not be nil");
-        XCTAssertNil(error, "error should be nil");
-        [expectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:15 handler:^(NSError *error) {
-        if (error != nil) {
-            NSLog(@"Error: %@", error.localizedDescription);
-        }
-    }];
+    XCTAssert(YES, @"Pass");
 }
 
 - (void)testPerformanceExample {
